ef6461bedfc2dca6ccf93add536f913bb0dafc48
ArlindoSilva <arlindoas2@gmail.com>
2024-10-10 13:07:43 -0300

    Fazendo a primeira requisição à API socialbooks

---
+++ src/main/java/com/algaworks/socialbooks/aplicacao/Autor.java
@@ -0,0 +1,61 @@
+package com.algaworks.socialbooks.aplicacao;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import java.util.Date;
+import java.util.List;
+
+
+public class Autor {
+
+    private Long id;
+
+    private String nome;
+
+    private String nacionalidade;
+
+    @JsonFormat(pattern = "dd/MM/yyyy")
+    private Date nascimento;
+
+    private List<Livro> livros;
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getNacionalidade() {
+        return nacionalidade;
+    }
+
+    public void setNacionalidade(String nacionalidade) {
+        this.nacionalidade = nacionalidade;
+    }
+
+    public Date getNascimento() {
+        return nascimento;
+    }
+
+    public void setNascimento(Date nascimento) {
+        this.nascimento = nascimento;
+    }
+
+    public List<Livro> getLivros() {
+        return livros;
+    }
+
+    public void setLivros(List<Livro> livros) {
+        this.livros = livros;
+    }
+}
