a1c6f7e9da091b32b6d4daab2b4f4e4a4f01f014
ArlindoSilva <arlindoas2@gmail.com>
2024-10-10 13:52:21 -0300

    Encapsulando os métodos na classe LivrosClient e manipulando os objetos na classe Aplicacao

--- src/main/java/com/algaworks/socialbooks/aplicacao/Aplicacao.java
+++ src/main/java/com/algaworks/socialbooks/aplicacao/Aplicacao.java
@@ -1,26 +1,38 @@
 package com.algaworks.socialbooks.aplicacao;
 
+import com.algaworks.socialbooks.client.domain.Livro;
+import com.algaworks.socialbooks.client.domain.LivrosClient;
 import org.springframework.http.RequestEntity;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.client.RestTemplate;
 
 import java.net.URI;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.List;
 
 public class Aplicacao {
 
-    public static void main (String[] args) {
-        RestTemplate restTemplate = new RestTemplate();
+    public static void main (String[] args) throws ParseException {
+        LivrosClient cliente = new LivrosClient();
 
-        RequestEntity<Void> request = RequestEntity
-                .get(URI.create("http://localhost:8080/livros"))
-                .header("Authorization", "Basic YWxnYXdvcmtzOnMzbmg0")
-                .build();
+        List<Livro> listaLivros = cliente.listar();
 
-        ResponseEntity<Livro[]> response = restTemplate.exchange(request, Livro[].class);
-
-        for (Livro livro : response.getBody()) {
+        for (Livro livro : listaLivros) {
             System.out.println("Livro: " + livro.getNome());
         }
+
+        Livro livro = new Livro();
+        livro.setNome("Git passo-a-passo");
+        livro.setEditora("Algaworks");
+
+        SimpleDateFormat publicacao = new SimpleDateFormat("dd/MM/yyyy");
+        livro.setPublicacao(publicacao.parse("10/05/2024"));
+
+        livro.setResumo("Git descomplicado para devs.");
+        String localizacao = cliente.salvar(livro);
+        System.out.println("URI do livro salvo: " + localizacao);
+
 
     }
 
